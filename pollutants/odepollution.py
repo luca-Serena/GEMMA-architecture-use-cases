a8aab38f65f7fabb4f73a9192a2bb5bacca64e58
luca-Serena <lucaserena35@gmail.com>
2022-12-31 18:09:58 +0100

    chenge in local consistency function

--- pollutants/odepollution.py
+++ pollutants/odepollution.py
@@ -9,6 +9,20 @@ from scipy.integrate import odeint
 #σ change from petroil to electric
 #γ change from GPL to electric
 
+
+def valuesRounding (howMany, valueList):
+	returnList = [int(elem) for elem in valueList]         # list of values to return. Continuous values are discretized
+	tempList = [elem - int(elem) for elem in valueList]    # temporary list with only the fractional part of the considered values
+	for i in range (howMany):							   # howMany depends on the total loss
+		max_value = max(tempList)						   # number with the highest fractional part
+		max_index = tempList.index(max_value)			   # index with of the number with the highest fractional part
+		tempList[max_index] = -1						   # to ignore in the future
+		returnList [max_index] += 1					       # element of the list rounded up 
+
+	return returnList
+
+
+
 def pollution_model(state : tuple, time : np.ndarray, 
                β : float, σ : float, γ : float) -> tuple:  #β change from petroil to GPL
     petroil, GPL, electric = state
@@ -28,19 +42,12 @@ def run (petroil, GPL, electric): #, duration, beta):
 	res = odeint(pollution_model, y0=state0, t=time, args=(β, σ, γ))
 	petroil_hat, GPL_hat, electric_hat = zip(*res)
 	
-	total_petroil = int(petroil_hat[-1])
-	total_GPL = int(GPL_hat[-1])
-	total_electric = int(electric_hat[-1])
+	total_petroil = petroil_hat[-1]
+	total_GPL = GPL_hat[-1]
+	total_electric = electric_hat[-1]
 
 	#local consistency control to manage loss due to conversion from continuous to discrete values
 
-	continuous_loss = (petroil + GPL + electric) - (total_petroil + total_GPL + total_electric)  
+	continuous_loss = (petroil + GPL + electric) - (int(total_petroil) + int(total_GPL) + int(total_electric)) 
 
-	if continuous_loss > 0:  #add the loss from continuous to discrete to the biggest compartment
-	    if total_petroil >= total_GPL and total_petroil >= total_electric:
-	        total_petroil += continuous_loss
-	    elif total_GPL >= total_electric:
-	        total_GPL += continuous_loss
-	    else:
-	        total_electric += continuous_loss
-	return total_petroil, total_GPL, total_electric
+	return valuesRounding (continuous_loss, [total_petroil, total_GPL, total_electric])
